d0846e8aa6de9b1cdf1e56da3d3f546fa152ec73
krupali pilgulwar <rpkrupali@gmail.com>
2022-01-30 08:38:53 -0600

    potion update

---
+++ __test__/Potion.test.js
@@ -0,0 +1,14 @@
+const Potion = require('../lib/Potion.js');
+
+test('Creates helth potion object', () => {
+    const potion = new Potion('health');
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+});
+
+test('create a random potion object',() => {
+    const potion = new Potion();
+    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name.length).toBeGreaterThan(0);
+    expect(potion.value).toEqual(expect.any(Number));
+})
